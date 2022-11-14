bd4af6f0e956ad9dd00f56e688e5211db50240ed
lidongjun <lidongjun@hnnewsoft.com>
2022-11-14 16:08:04 +0800

    常用命令

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@
 ./gradlew --stacktrace  auto-patch-plugin:uploadArchives
 ./gradlew --stacktrace gradle-plugin:uploadArchives
 ./gradlew --stacktrace patch:uploadArchives
+
+#打原生包和补丁包时应用
+./gradlew  assembleDebug  -Dorg.gradle.daemon=false -Dorg.gradle.debug=false --stacktrace
+#测试时补丁上传命令
+ adb push app/build/outputs/robust/patch.jar /storage/emulated/0/Android/data/com.meituan.robust.sample/files/robust/patch.jar
